191bf2071b181ab7b8649413d09a1c0b50874de1
Jérémy Gendre <gendrejeremy@yahoo.fr>
2022-10-03 22:17:58 +0200

    fix: createRoot usage and fix ts types

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import {auth} from "./config/firebase";
 import UserContextProvider from "./context/UserContext";
 import {BrowserRouter} from "react-router-dom";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
   <React.StrictMode>
       <UserContextProvider>
           <BrowserRouter>
               <App />
           </BrowserRouter>
       </UserContextProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change

a04c292980bde852bff2ad6a5c333cd21ba1fa7c
Jérémy Gendre <gendrejeremy@yahoo.fr>
2022-10-07 16:53:17 +0200

    feat: search user globally by nickname

--- src/component/Layout/GlobalNicknameSearch.tsx
+++ src/component/Layout/GlobalNicknameSearch.tsx
@@ -1,33 +1,82 @@
-import {InputAdornment, TextField} from "@mui/material";
+import {Autocomplete, CircularProgress, InputAdornment, TextField} from "@mui/material";
 import { styled } from '@mui/material/styles';
 import SearchIcon from '@mui/icons-material/Search';
-import React, {FormEvent, useState} from "react";
+import React, {FormEvent, useEffect, useState} from "react";
+import {useDebounceValue} from "@jeremygendre/react-custom-hooks";
+import { query, collection, getDocs, where } from "firebase/firestore";
+import {buildCollectionFromSnapshot, db} from "../../config/firebase";
+import {BlacklistedUser} from "../../models/BlacklistedUser";
+import {useNavigate} from "react-router";
 
 export default function GlobalNicknameSearch(){
+    const [open, setOpen] = React.useState(false);
+    const [options, setOptions] = React.useState<readonly BlacklistedUser[]>([]);
+    const [loading, setLoading] = useState(false);
     const [search, setSearch] = useState('');
+    const debouncedValue = useDebounceValue(search);
+    const navigate = useNavigate();
 
-    const handleSubmit = (e: FormEvent) => {
+    const searchFnc = async () => {
+        console.log(debouncedValue);
+        setLoading(true);
+        const q = query(collection(db, "BlacklistedUser"), where("nickname", "==", debouncedValue));
+        const querySnapshot = await getDocs(q);
+        setOptions(buildCollectionFromSnapshot(querySnapshot));
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        console.log(debouncedValue);
+        searchFnc();
+    },[debouncedValue]);
+
+    const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
-        console.log(search);
+        searchFnc();
     };
 
     return (
         <form onSubmit={handleSubmit}>
-            <CssTextField
-                id="global-search"
-                placeholder="Global nickname search..."
-                InputProps={{
-                    startAdornment: (
-                        <InputAdornment sx={{color: 'white'}} position="start">
-                            <SearchIcon />
-                        </InputAdornment>
-                    ),
+            {/**/}
+            <Autocomplete
+                id="autocomplete-search"
+                sx={{ width: 300 }}
+                open={open}
+                onOpen={() => {setOpen(true);}}
+                onClose={() => {setOpen(false);}}
+                getOptionLabel={(option) => option.nickname + ` (${option.sourceUid})`}
+                options={options}
+                onChange={(event: any, newValue:BlacklistedUser|null) => {
+                    if(newValue){
+                        navigate(`/list/${newValue.sourceUid}`)
+                    }
                 }}
-                variant="outlined"
+                loading={loading}
                 size="small"
-                required
-                value={search}
-                onChange={e => setSearch(e.target.value)}
+                renderInput={(params) => (
+                    <CssTextField
+                        placeholder="Global nickname search..."
+                        variant="outlined"
+                        required
+                        value={search}
+                        onChange={e => setSearch(e.target.value)}
+
+                        {...params}
+                        InputProps={{
+                            ...params.InputProps,
+                            startAdornment: (
+                                <InputAdornment sx={{color: 'white'}} position="start">
+                                    <SearchIcon />
+                                </InputAdornment>
+                            ),
+                            endAdornment: (
+                                <React.Fragment>
+                                    {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                                </React.Fragment>
+                            ),
+                        }}
+                    />
+                )}
             />
         </form>
     );
